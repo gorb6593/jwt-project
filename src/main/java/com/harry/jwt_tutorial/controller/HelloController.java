5bf143683b6b9ef24b7ea9304e042f248f2caef3
gorb6593 <gorb6593@gmail.com>
2024-11-18 21:57:11 +0900

    jwt프로젝트 기초 세팅

---
+++ src/main/java/com/harry/jwt_tutorial/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.harry.jwt_tutorial.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class HelloController {
+  @GetMapping("/hello")
+  public ResponseEntity<String> hello() {
+    return ResponseEntity.ok("hello");
+  }
+}
